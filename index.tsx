82d050c50cb0b061d433e4fb792e5acfcac75195
Leandro Klyte <klyte45@hotmail.com>
2024-04-15 20:49:17 -0300

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,16 @@
+///<reference path="../../_replacements/euis-components/types/euis.d.ts" />
+///<reference path="../../_replacements/euis-components/types/cohtml.d.ts" />
+///<reference path="../../_replacements/euis-components/types/declarations.d.ts" />
+
+
+export * from "./src/service/DistrictRelativeService"
+export * from "./src/service/GitHubAddressesFilesSevice"
+export * from "./src/service/NamesetService"
+export * from "./src/service/NamingRulesService"
+export * from "./src/service/SelectInfoPanelService"
+export * from "./src/service/EditorUISystemService"
+
+export * from "../../_replacements/euis-components/src/utility/Entity"
+export * from "../../_replacements/euis-components/src/utility/name.utils" 
+
+export * from "./src/model/SelectInfoPanelModels"
