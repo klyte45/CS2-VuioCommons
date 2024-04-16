cdae1fda1f0bbd74b120f8c341edcd59fbf51fda
Leandro Klyte <klyte45@hotmail.com>
2024-04-15 21:39:39 -0300

    Adding common files

--- index.tsx
+++ index.tsx
@@ -3,14 +3,4 @@
 ///<reference path="../../_replacements/euis-components/types/declarations.d.ts" />
 
 
-export * from "./src/service/DistrictRelativeService"
-export * from "./src/service/GitHubAddressesFilesSevice"
-export * from "./src/service/NamesetService"
-export * from "./src/service/NamingRulesService"
-export * from "./src/service/SelectInfoPanelService"
-export * from "./src/service/EditorUISystemService"
-
-export * from "../../_replacements/euis-components/src/utility/Entity"
-export * from "../../_replacements/euis-components/src/utility/name.utils" 
-
-export * from "./src/model/SelectInfoPanelModels"
+export * from './src/VanillaComponentResolver'
