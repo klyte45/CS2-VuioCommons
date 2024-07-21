abacd3c9e26c22dafa5107c0a82027e5d682f46b
Leandro Klyte <klyte45@hotmail.com>
2024-07-21 15:53:35 -0300

    Added hierarchy viewport

--- src/VanillaWidgets.tsx
+++ src/VanillaWidgets.tsx
@@ -1,7 +1,7 @@
 
-import { DropdownItem, Theme, UniqueFocusKey } from "cs2/bindings";
+import { DropdownItem, LocalizedString, LocElement, Theme, UniqueFocusKey } from "cs2/bindings";
 import { getModule } from "cs2/modding";
-import { HTMLAttributes } from "react";
+import { CSSProperties, HTMLAttributes } from "react";
 import { VanillaComponentResolver } from "./VanillaComponentResolver";
 
 export type UIColorRGBA = {
@@ -10,6 +10,13 @@ export type UIColorRGBA = {
     b: number
     a: number
 }
+export enum LocElementType {
+    Bounds = "Game.UI.Localization.LocalizedBounds",
+    Fraction = "Game.UI.Localization.LocalizedFraction",
+    Number = "Game.UI.Localization.LocalizedNumber",
+    String = "Game.UI.Localization.LocalizedString"
+}
+
 type PropsColorPicker = {
     label?: string | JSX.Element | JSX.Element[]
     value: UIColorRGBA
@@ -49,7 +56,7 @@ type PropsDropdownField<T> = {
     disabled?: boolean
 } & Omit<HTMLAttributes<any>, "onChange">
 
-type PropsEditorItemControl = { label: string, children?: JSX.Element | JSX.Element[] | string, styleContent?: React.CSSProperties }
+type PropsEditorItemControl = { label?: string, children?: JSX.Element | JSX.Element[] | string, styleContent?: React.CSSProperties }
 type PropsFocusableEditorItem = { disabled?: boolean, centered?: boolean, className?: string, focusKey?: UniqueFocusKey, onFocusChange?: () => any, children?: JSX.Element | JSX.Element[] | string }
 type PropsDirectoryPickerButton = { label: string, value: string, disabled?: boolean, className?: string, theme?: Theme, onOpenDirectoryBrowser: () => any }
 type PropsStringInputField = { value: string, disabled?: boolean, onChange: (s: string) => any, className?: string, maxLength?: number } & ({
@@ -64,6 +71,27 @@ type PropsFloatInputField = { label: string, value: number, min?: number, max?: 
     onChangeStart?: HTMLInputElement['onfocus'], onChangeEnd?: HTMLInputElement['onblur'], multiline?: false | undefined,
 })
 
+export type HierarchyViewport = {
+    displayName: LocElement,
+    icon?: string,
+    tooltip?: LocElement,
+    level: number,
+    selectable?: boolean,
+    selected?: boolean,
+    expandable?: boolean,
+    expanded?: boolean
+}
+
+type PropsHierarchyMenu = {
+    viewport: HierarchyViewport[],
+    flex: { grow: CSSProperties['flexGrow'], shrink: CSSProperties['flexShrink'], basis: CSSProperties['flexBasis'] },
+    visibleCount: number,
+    onSelect?: (viewportIndex: number, selected: boolean) => any,
+    onSetExpanded?: (viewportIndex: number, expanded: boolean) => any,
+    singleSelection?: boolean,
+    onRenderedRangeChange?: (startIndex: number, endIndex: number) => any
+}
+
 const registryIndex = {
     themeDropdown: ["game-ui/menu/widgets/dropdown-field/dropdown-field.module.scss", "classes"],
     inputField: ["game-ui/debug/widgets/fields/input-field/input-field.module.scss", "classes"],
@@ -77,6 +105,7 @@ const registryIndex = {
     StringInputField: ["game-ui/editor/widgets/fields/string-input-field.tsx", "StringInputField"],
     ToggleField: ["game-ui/editor/widgets/fields/toggle-field.tsx", "ToggleField"],
     FloatInputField: ["game-ui/editor/widgets/fields/float-input-field.tsx", "FloatInputField"],
+    HierarchyMenu: ["game-ui/editor/widgets/hierarchy-menu/hierarchy-menu.tsx", "HierarchyMenu"],
 }
 
 
@@ -105,14 +134,14 @@ export class VanillaWidgets {
     public get FocusableEditorItem(): (props: PropsFocusableEditorItem) => JSX.Element { return this.cachedData["FocusableEditorItem"] ?? this.updateCache("FocusableEditorItem") }
     public get editorItemModule(): Theme | any { return this.cachedData["editorItemModule"] ?? this.updateCache("editorItemModule") }
     public EditorItemRow = ({ label, children, styleContent }: PropsEditorItemControl) => <VanillaWidgets.instance.FocusableEditorItem focusKey={VanillaComponentResolver.instance.FOCUS_DISABLED}>
-        <div className={this.editorItemModule.row}>
-            <div className={this.editorItemModule.label}>{label}</div>
-            <div className={this.editorItemModule.control} style={styleContent} >{children}</div>
+        <div className={this.editorItemModule.row} style={label ? {} : styleContent}>
+            {label ? <><div className={this.editorItemModule.label}>{label}</div><div className={this.editorItemModule.control} style={styleContent}>{children}</div></> : children}
         </div>
     </VanillaWidgets.instance.FocusableEditorItem>
     public get DirectoryPickerButton(): (props: PropsDirectoryPickerButton) => JSX.Element { return this.cachedData["DirectoryPickerButton"] ?? this.updateCache("DirectoryPickerButton") }
     public get StringInputField(): (props: PropsStringInputField) => JSX.Element { return this.cachedData["StringInputField"] ?? this.updateCache("StringInputField") }
     public get ToggleField(): (props: PropsToggleField) => JSX.Element { return this.cachedData["ToggleField"] ?? this.updateCache("ToggleField") }
     public get FloatInputField(): (props: PropsFloatInputField) => JSX.Element { return this.cachedData["FloatInputField"] ?? this.updateCache("FloatInputField") }
+    public get HierarchyMenu(): (props: PropsHierarchyMenu) => JSX.Element { return this.cachedData["HierarchyMenu"] ?? this.updateCache("HierarchyMenu") }
 
 }
